d6120648655427f9753976717b1543bbf459d0d8
Chang Zhao <250151700@qq.com>
2018-11-17 17:37:13 +0800

    (feat)抽奖逻辑实现，能够返回数据 url:/draw, 方法post 目前有明显bug，若中奖人数大于1，有可能会抽取同一个qq号

--- app/Http/Controllers/LottoryController.php
+++ app/Http/Controllers/LottoryController.php
@@ -11,38 +11,27 @@ class LottoryController extends Controller
         return view('lottory.create');
     }
 
-    function checkLabel(Record $record,Request $request){
-        $ret = true;
-        
-        if (strops(($record->label),$requset->label) == FALSE){
-            $ret = false;
-        }
-        
-        return $ret;
-    }
-
-    function checkRestrictCond(Record $record,Request $request){
-        return true;
-    }
-
+    
     public function store(Request $request)
     {
         $validRecords = [];  // 抽奖池
         $records = Record::all(); // select *
         
-        foreach ($records as $record) // 遍历
+        // date_create函数，把格式为 YYYY-MM-DD HH:MM:SS 的字符串转换为一个datetime对象
+        $startDate = date_create($request->start); // 抽奖的起始时间，在这个范围内的聊天记录才会被筛查
+        $endDate = date_create($request->end);
+
+        foreach ($records as $record) // 遍历聊天记录
         {
+            $recordDate = date_create($record->time); 
             
-            $date = date_create($record->time); // 将数据库里的聊天记录的发言时间，从 YYYY-MM-DD HH:MM:SS 字符串形式转换为一个datetime对象
-            
-            if (($date >= $request->start) &&
-                ($date <= $request->end))  // 筛选存在于抽奖时间段之内的聊天记录
+            if (($recordDate >= $startDate) &&
+                ($recordDate <= $endDate))  // 筛选存在于抽奖时间段之内的聊天记录
                 {
-                    echo "yes";
-                    if ((checkLabel($record, $request)) // 发言记录要包含抽奖标签
-                        and (checkRestrictCond($record, $request)))
+                    if (($this->checkLabel($record, $request)) // 发言记录要包含抽奖标签
+                        and ($this->checkRestrictCond($record, $request)))
                     {
-                        $validRecords.append($record);
+                        array_push($validRecords,$record);
                     }
                 }
         }
@@ -56,12 +45,28 @@ class LottoryController extends Controller
             return $luckyDog;
         }
         // 如果没进上面的if分支，意味着参与抽奖的人数大于可中奖人数。
-        for ($ii=0; ($ii < $request->people_number); $$ii++)
+        
+        for ($ii=0; $ii < $request->people_number; $ii++)
         {
             $randNum = mt_rand(0, count($validRecords)-1);
-            $luckyDog.append($validRecords[$randNum]->qqnumber);
+            //$luckyDog.append($validRecords[$randNum]->qqnumber);
+            array_push($luckyDog, $validRecords[$randNum]->qqnumber);
             array_splice($validRecords, $randNum,1);
         }
         return $luckyDog;
     }
+    function checkLabel(Record $record,Request $request){
+        $ret = true;
+        
+        if (strpos(($record->label),$request->label) == FALSE){
+            $ret = false;
+        }
+        
+        return $ret;
+    }
+
+    function checkRestrictCond(Record $record,Request $request){
+        return true;
+    }
+
 }
